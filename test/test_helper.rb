716110721bac9c2d6529fec3d7edf5f63f67e07c
Eric-Guo <eric.guocz@gmail.com>
2019-09-21 16:40:14 +0800

    Initial version for Shopify Hackathon Shanghai on Rails 5

---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+  end
+end
+
+module ActionDispatch
+  class IntegrationTest
+    include Devise::Test::IntegrationHelpers
+  end
+end
