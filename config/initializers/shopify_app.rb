a5b4f787f9f6420d28ccc31697cd40c10e685e38
Eric-Guo <eric.guocz@gmail.com>
2019-09-21 17:08:49 +0800

    After running rails generate shopify_app:install

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,14 +1,14 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
-  config.api_key = ENV['SHOPIFY_API_KEY']
-  config.secret = ENV['SHOPIFY_API_SECRET']
+  config.application_name = "eric-test"
+  config.api_key = Rails.application.credentials.shopify_api_key
+  config.secret = Rails.application.credentials.shopify_api_secret
   config.old_secret = ""
   config.scope = "read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2019-07"
-  config.session_repository = Shop
+  config.session_repository = ShopifyApp::InMemorySessionStore
 end
 
 # ShopifyApp::Utils.fetch_known_api_versions                        # Uncomment to fetch known api versions from shopify servers on boot
