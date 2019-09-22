4a2c60e97764bd5308c6f465b7033517d734008c
Eric-Guo <eric.guocz@gmail.com>
2019-09-22 11:55:55 +0800

    Add write_script_tags functions

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,12 +3,16 @@ ShopifyApp.configure do |config|
   config.api_key = Rails.application.credentials.shopify_api_key
   config.secret = Rails.application.credentials.shopify_api_secret
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_products, write_script_tags" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2019-07"
   config.session_repository = ShopifyApp::InMemorySessionStore
+  config.scripttags = [{
+    event: 'onload',
+    src: "https://ericguo.ggrok.com/welcome_bar.js"
+  }]
 end
 
 # ShopifyApp::Utils.fetch_known_api_versions                        # Uncomment to fetch known api versions from shopify servers on boot
